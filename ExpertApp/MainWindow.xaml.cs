940c077877495fb6f091dd80c7252373d8deb9b2
rafalsosnowski <rafalsosnowski4@wp.pl>
2022-04-28 23:07:39 +0200

    ExpretFrontendInit

--- ExpertApp/MainWindow.xaml.cs
+++ ExpertApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 
 namespace ExpertApp
 {
@@ -10,6 +11,27 @@ namespace ExpertApp
         public MainWindow()
         {
             InitializeComponent();
+            this.FirstPreference.Items.Clear();
+            this.SecondPreference.Items.Clear();
+            this.FirstPreference.ItemsSource = Preferences;
+            this.SecondPreference.ItemsSource = Preferences;
+            this.ProcessMethod.ItemsSource = ProcessMethods;
+        }
+        List<string> Preferences = new List<string>{ "Pref1", "Pref2" }, ProcessMethods = new List<string>{ "Method1", "Method2" };
+        private void Save(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show(this, (string)FirstPreference.SelectedItem+ (string)ProcessMethod.SelectedItem + (string)SecondPreference.SelectedItem + NewPreferenceName.Text,
+"nono", MessageBoxButton.OK, MessageBoxImage.Information);
+            if(false)
+            {
+                MessageBox.Show(this, "bad as fuck",
+"Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+            Preferences.Add(this.NewPreferenceName.Text);
+            this.FirstPreference.Items.Refresh();
+            this.SecondPreference.Items.Refresh();
         }
     }
 }
