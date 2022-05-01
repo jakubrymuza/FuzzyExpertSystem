9db199c050ab3529efe6503399d66232cf5d844b
szymmat <matszymczak00@wp.pl>
2022-05-01 20:41:37 +0200

    Added quiz question handling

---
+++ Backend/Core/QuizQuestions/QuizQuestion.cs
@@ -0,0 +1,14 @@
+﻿namespace Backend.Core.QuizQuestions
+{
+    internal class QuizQuestion
+    {
+        public string Name { get; }
+        public string Description { get; }
+
+        public QuizQuestion(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
