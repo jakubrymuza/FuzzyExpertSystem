b341544137b09958335123183f0c51707c5dfe99
rafalsosnowski <rafalsosnowski4@wp.pl>
2022-05-08 22:16:35 +0200

    Expert Frontend business finished

--- ExpertApp/MainWindow.xaml.cs
+++ ExpertApp/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using Backend.Core;
+using Backend.Core.CalculatingEngines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 
 namespace ExpertApp
@@ -7,31 +11,62 @@ namespace ExpertApp
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {        
+        InferenceEngine engine; 
         public MainWindow()
         {
             InitializeComponent();
-            this.FirstPreference.Items.Clear();
-            this.SecondPreference.Items.Clear();
-            this.FirstPreference.ItemsSource = Preferences;
-            this.SecondPreference.ItemsSource = Preferences;
-            this.ProcessMethod.ItemsSource = ProcessMethods;
+            engine = InferenceEngine.GetInstance();
+            RefreshAllRulesLists();
+            this.FirstPreference.SelectedIndex = 0;
+            this.ProcessMethod.SelectedIndex = 0;
+            this.SecondPreference.SelectedIndex = 0;
+            this.RulesToEdit.SelectedIndex = 0;
+
+            this.ProcessMethod.ItemsSource = Enum.GetValues(typeof(OperatorType)).Cast<OperatorType>();
+            
         }
-        List<string> Preferences = new List<string> { "Pref1", "Pref2" }, ProcessMethods = new List<string> { "Method1", "Method2" };
         private void Save(object sender, RoutedEventArgs e)
         {
 
-            MessageBox.Show(this, (string)FirstPreference.SelectedItem + (string)ProcessMethod.SelectedItem + (string)SecondPreference.SelectedItem + NewPreferenceName.Text,
-"nono", MessageBoxButton.OK, MessageBoxImage.Information);
-            if (false)
+            if (NewPreferenceName.Text == "")
             {
-                MessageBox.Show(this, "bad as fuck",
-"Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show(this, "Nie podano nazwy reguły","Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            else
+            {
+                engine.AddRule(new Backend.Core.Evaluables.Rule(NewPreferenceName.Text, (string)FirstPreference.SelectedItem, (string)SecondPreference.SelectedItem, (OperatorType)ProcessMethod.SelectedItem, false));
+                SetRoot(NewPreferenceName.Text);
+                RefreshAllRulesLists();
+            }
+        }
 
-            Preferences.Add(this.NewPreferenceName.Text);
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            engine.RemoveRule((string)this.RulesToEdit.SelectedItem);
+            RefreshAllRulesLists();
+        }
+        private void RootButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetRoot((string)this.RulesToEdit.SelectedItem);
+        }
+        private void RefreshAllRulesLists()
+        {
+            var Preferences = engine.GetRuleNames().Select(x => x.Name);
+            var Rules = engine.GetRuleNames().Select(x => x.Name);
+            this.FirstPreference.ItemsSource = Preferences;
+            this.SecondPreference.ItemsSource = Preferences;
+            this.RulesToEdit.ItemsSource = Rules;
             this.FirstPreference.Items.Refresh();
             this.SecondPreference.Items.Refresh();
+            this.RulesToEdit.Items.Refresh();
         }
+        private void SetRoot(string name)
+        {
+            //engine.SetNewRoot(name);
+            CurrentRoot.Text=name;
+        }
+
+        
     }
 }
