32e77caba604d276ec44a5a53dd9d0223e4d8fed
Karol Nowiński <01151604@pw.edu.pl>
2022-05-10 00:19:21 +0200

    client frontend

--- ClientApp/MainWindow.xaml.cs
+++ ClientApp/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
 ﻿using System.Windows;
+using Backend.Core;
+using System.Collections.Generic;
+using Backend.Core.QuizAnswers;
+using Backend.Core.QuizQuestions;
+using Backend.Data;
+using System;
 
 namespace ClientApp
 {
@@ -10,6 +16,56 @@ namespace ClientApp
         public MainWindow()
         {
             InitializeComponent();
+            KnowledgeBase knowledgebase = KnowledgeBase.GetInstance();
+            var questions = knowledgebase.GetQuestions();
+            
+            List<QuizItem> items = new List<QuizItem>();
+            foreach (var question in questions) {
+                items.Add(new QuizItem(question.Name, question.Description));
+            }
+
+
+            UserFormList.ItemsSource = items;
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SuggestionListView.ItemsSource = null;
+
+            List<IQuizAnswer> answerList = new List<IQuizAnswer>();
+
+            foreach (QuizItem tmp in UserFormList.Items) {
+                answerList.Add(new QuizAnswer(tmp.name,tmp.val));
+            }
+
+            InferenceEngine engine = InferenceEngine.GetInstance();
+
+            var TripsList = engine.GetSortedTrips(answerList);
+
+
+            SuggestionListView.ItemsSource = TripsList;
+
+
+            return;
+        }
+
+
     }
+
+
+    public class QuizItem {
+
+        public int val { get; set; }
+        public string description { get; }
+        public string name { get; }
+
+
+        public QuizItem(string Name, string Desciption) {
+            val = 1;
+            name = Name;
+            description = Desciption;
+        }
+
+    }
+
 }
